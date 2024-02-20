91194e9502b6d3ef11b6263918928107314c7569
huseyinkedik <159181717+huseyinkedik@users.noreply.github.com>
2024-02-20 07:40:55 +0300

    Eksik yükleme düzeltildi

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int yil;
+
+        // Kullanıcı girişi
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Yılı giriniz: ");
+        yil = inp.nextInt();
+
+        // Artık yıl 4'e kalansız bölünür
+        //100'ün katıysa 400'e tam bölünür
+
+        if (yil % 4 == 0 && (yil % 100 != 0 || yil % 400 == 0)) {
+            System.out.println(yil + " artık yıldır");
+        } else {
+            System.out.println(yil + " artık yıl değildir");
+        }
+    }
 }
